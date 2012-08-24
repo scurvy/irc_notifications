0a72f93f072c6017e5aeb4f5144e94e4f5ed3c17
Richard Hesse <diq@weebly.com>
2012-08-24 23:37:14 +0000

    remove crap from previous campfire crap script. works with new rails now.

--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -6,7 +6,7 @@ class NotifierHook < Redmine::Hook::Listener
     @project = context[:project]
     @issue = context[:issue]
     @user = @issue.author
-    say "#{@user.login} created issue “#{@issue.subject}” Comment: #{truncate_words(@issue.description)} #{PROTO}://#{Setting.host_name}/issues/#{@issue.id}"
+    say "#{@user.login} created issue #{@issue.subject} Comment: #{truncate_words(@issue.description)} #{PROTO}://#{Setting.host_name}/issues/#{@issue.id}"
   end
   
   def controller_issues_edit_after_save(context = { })
@@ -15,11 +15,11 @@ class NotifierHook < Redmine::Hook::Listener
     @journal = context[:journal]
     @user = @journal.user
     if @issue.closed? == true
-      say "#{@user.login} closed issue “#{@issue.subject}” Comment: #{truncate_words(@journal.notes)} #{PROTO}://#{Setting.host_name}/issues/#{@issue.id}"
+      say "#{@user.login} closed issue #{@issue.subject} Comment: #{truncate_words(@journal.notes)} #{PROTO}://#{Setting.host_name}/issues/#{@issue.id}"
     elsif @issue.reopened? == true
-      say "#{@user.login} reopened issue “#{@issue.subject}” Comment: #{truncate_words(@journal.notes)} #{PROTO}://#{Setting.host_name}/issues/#{@issue.id}"
+      say "#{@user.login} reopened issue #{@issue.subject} Comment: #{truncate_words(@journal.notes)} #{PROTO}://#{Setting.host_name}/issues/#{@issue.id}"
     else
-      say "#{@user.login} updated issue “#{@issue.subject}” Comment: #{truncate_words(@journal.notes)} #{PROTO}://#{Setting.host_name}/issues/#{@issue.id}"
+      say "#{@user.login} updated issue #{@issue.subject} Comment: #{truncate_words(@journal.notes)} #{PROTO}://#{Setting.host_name}/issues/#{@issue.id}"
     end
   end
 
@@ -27,21 +27,21 @@ class NotifierHook < Redmine::Hook::Listener
     @project = context[:project]
     @message = context[:message]
     @user = @message.author
-    say "#{@user.login} wrote a new message “#{@message.subject}” on #{@project.name}: #{truncate_words(@message.content)}. #{PROTO}://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    say "#{@user.login} wrote a new message #{@message.subject} on #{@project.name}: #{truncate_words(@message.content)}. #{PROTO}://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
   end
   
   def controller_messages_reply_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
     @user = @message.author
-    say "#{@user.login} replied a message “#{@message.subject}” on #{@project.name}: #{truncate_words(@message.content)}. #{PROTO}://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    say "#{@user.login} replied a message #{@message.subject} on #{@project.name}: #{truncate_words(@message.content)}. #{PROTO}://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
   end
   
   def controller_wiki_edit_after_save(context = { })
     @project = context[:project]
     @page = context[:page]
     @user = @page.content.author
-    say "#{@user.login} edited the wiki “#{@page.pretty_title}” on #{@project.name} #{PROTO}://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
+    say "#{@user.login} edited the wiki #{@page.pretty_title} on #{@project.name} #{PROTO}://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
   end
 
 private
@@ -53,7 +53,7 @@ private
     end
   end
 
-  def truncate_words(text, length = 45, end_string = '…')
+  def truncate_words(text, length = 45, end_string = '...')
     return if text == nil
     words = text.split()
     words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
